4331e14676aa4a650e401316b3be627fad136de4
AntonioDeveloper <juniorgabrielsantana@hotmail.com>
2021-04-04 03:05:54 -0300

    Teste

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ const cors = require('cors');
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+  'https://nossa-pizza-frontend.herokuapp.com/'
+));
 
 //ROUTES
 const clientRoute = require('./routes/clientScripts');
